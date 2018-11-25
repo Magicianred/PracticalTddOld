be9d87300b95fe4c20aaf4a933d7ccecc7fa8511
lucapiccinelli <luca.picci@gmail.com>
2018-11-25 18:24:42 +0100

    all tests green

--- app/Program.cs
+++ app/Program.cs
@@ -14,16 +14,34 @@ namespace app
                 .Select(frameAsStr => Array.ConvertAll(frameAsStr, rollStr => Convert.ToInt32(rollStr)))
                 .ToArray();
 
-            KeyValuePair<int, int>[] framesPartialScore = frames
+            List<int[]> standardFrames = frames.Where(frame => frame.Length < 3).ToList();
+            int[] lastFrame = frames.Where(frame => frame.Length == 3).FirstOrDefault() ?? new int[0];
+
+            if(lastFrame.Length > 0)
+            {
+                if(lastFrame.First() == 10)
+                {
+                    standardFrames.Add(new int[]{lastFrame.First()});
+                    standardFrames.Add(new int[]{lastFrame[1]});
+                    standardFrames.Add(new int[]{lastFrame[2]});
+                }
+                else
+                {
+                    standardFrames.Add(new int[]{lastFrame[0], lastFrame[1]});
+                    standardFrames.Add(new int[]{lastFrame.Last()});
+                }
+            }
+
+            KeyValuePair<int, int>[] framesPartialScore = standardFrames
                 .Select(frame => KeyValuePair.Create((frame.Length % 2) + 1, frame.Sum()))
                 .ToArray();
 
-            int[] framesScore = new int[framesPartialScore.Length];
-            Array.Copy(framesPartialScore.Select(partialScore => partialScore.Value).ToArray(), framesScore, framesPartialScore.Length);
+            int length = framesPartialScore.Length > 10 ? framesPartialScore.Length - 1 : framesPartialScore.Length;
+            int[] framesScore = new int[length];
+            Array.Copy(framesPartialScore.Select(partialScore => partialScore.Value).ToArray(), framesScore, length);
 
-            for (int i = 0; i < framesPartialScore.Length; i++)
+            for (int i = 0; i < framesPartialScore.Length && i < 10; i++)
             {
-
                 int score = framesPartialScore[i].Value;
                 int framesToAdd = framesPartialScore[i].Key;
                 if (score == 10)
@@ -44,11 +62,13 @@ namespace app
             }
 
             int totalScore = framesScore
+                .Take(10)
                 .TakeWhile(score => score > 0)
                 .Sum();
 
             Console.WriteLine(args[1]);
             Console.WriteLine(framesScore
+                .Take(10)
                 .Take(framesScore.TakeWhile(score => score > 0).Count() + 1)
                 .Aggregate("", (acc, rollScore) => $"{acc}{(acc == string.Empty ? string.Empty : ",")}{(rollScore < 0 ? "-" : rollScore.ToString())}"));
             Console.WriteLine(totalScore > 0 ? totalScore.ToString() : "-");
--- tests/AsAPlayer_IWant_ToKnow_MyBowlingPoints.cs
+++ tests/AsAPlayer_IWant_ToKnow_MyBowlingPoints.cs
@@ -9,17 +9,16 @@ namespace tests
     {
         [Theory]
         [InlineData("10,10,10,10,10,10,10,10,10,10-10-10", "30,30,30,30,30,30,30,30,30,30", "300")] // All strikes
+        [InlineData("10,10,10,10,10,10,10,10,10,5-5-10", "30,30,30,30,30,30,30,30,30,20", "290")] // Last frame is a Spare
         [InlineData("1-1,1-1,1-1,1-1,1-1,1-1,1-1,1-1,1-1,1-1", "2,2,2,2,2,2,2,2,2,2", "20")] // All open frames
         [InlineData("5-5,1-1", "12,2", "14")] // One closed Spare
         [InlineData("5-5,1-1,3-7,4-1", "12,2,15,5", "34")] // Two closed Spares
         [InlineData("5-5,3-7,4-1", "20,15,5", "40")] // Two consecutives closed Spares
         [InlineData("5-5", "-", "-")] // One open Spare
         [InlineData("10,8-0,1-3", "22,8,4", "34")] // One closed Strike
-        [InlineData("10,8-0,1-3", "22,8,4", "34")] // One closed Strike
         [InlineData("10,10,8-0,1-3", "28,22,8,4", "62")] // Two consecutives closed Strikes
         [InlineData("10,8-2,1-3", "24,14,4", "42")] // One closed Strike followed by one closed Spare
         [InlineData("8-2,10,1-3,2-5", "20,21,4,7", "52")] // One closed Spare followed by one closed Strike
-        [InlineData("8-2,10,1-3,2-5", "20,21,4,7", "52")] // One open Strike with one following roll
         [InlineData("10,4-5", "-", "-")] // One open Strike with one following roll
         [InlineData("10", "-", "-")] // One open Strike with no following roll
         [InlineData("10,5-5", "-", "-")] // One open Strike with one open Spare
